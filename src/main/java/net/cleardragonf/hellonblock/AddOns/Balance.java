f40efee252e7dc9ca74c375495137626c1191c5a
chrism339@hotmail.com <chrism339@hotmail.com>
2021-09-16 10:23:15 -0500

    •  Fully functional as of now for 1.16.5...need to work on configuration...and a few other changes.

--- src/main/java/net/cleardragonf/hellonblock/AddOns/Balance.java
+++ src/main/java/net/cleardragonf/hellonblock/AddOns/Balance.java
@@ -1,6 +1,8 @@
 package net.cleardragonf.hellonblock.AddOns;
 
 import net.cleardragonf.hellonblock.HOB;
+import net.kyori.adventure.text.Component;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandExecutor;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.exception.CommandException;
@@ -19,12 +21,11 @@ public class Balance implements CommandExecutor {
     @Override
     public CommandResult execute(CommandContext context) throws CommandException {
         if((context.cause().root() instanceof Player)){
-            Player player = (Player) context.cause().root();
-            Optional<UniqueAccount> uOpt = HOB.getEcon().findOrCreateAccount(player.uniqueId());
-            if (uOpt.isPresent()) {
-                UniqueAccount acc = uOpt.get();
-                BigDecimal balance = acc.balance(HOB.getEcon().defaultCurrency());
-            }
+            Player player = (Player)context.cause().root();
+
+            UUID player2 = player.uniqueId();
+            BigDecimal balance = HOB.getEcon().findOrCreateAccount(player2).get().balance(HOB.getEcon().defaultCurrency());
+            player.sendMessage(Component.text(balance.toString()));
         }
         return CommandResult.success();
     }
